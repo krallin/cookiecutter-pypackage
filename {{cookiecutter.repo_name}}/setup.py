8967663bbbd24d83a1d36032b3567388d12d7e2f
Thomas Orozco <thomas@orozco.fr>
2013-08-25 12:00:26 +0200

    Define version in a single location

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -16,10 +16,11 @@ if sys.argv[-1] == 'publish':
 
 readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+execfile('{{ cookiecutter.repo_name }}')  # Load __version__
 
 setup(
     name='{{ cookiecutter.repo_name }}',
-    version='{{ cookiecutter.version }}',
+    version=__version__,
     description='{{ cookiecutter.project_short_description }}',
     long_description=readme + '\n\n' + history,
     author='{{ cookiecutter.full_name }}',
