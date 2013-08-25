2de6af05daf22fadbf46225f6617943bb69250dd
Thomas Orozco <thomas@orozco.fr>
2013-08-25 12:15:53 +0200

    Add warning in the version file

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/version.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/version.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# This file is executed by setup.py to load the version without installing the package.
+# Make sure you do not add any side effects to this file.
+
 __version__ = '{{ cookiecutter.version }}'
